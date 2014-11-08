88e41fb47a5970fce861f025f8504cdf9519046f
iKerry <wr_asdf@163.com>
2014-11-08 22:56:20 +0800

    [Kerry] add animation for slide description

--- app/scripts/mobile/main.js
+++ app/scripts/mobile/main.js
@@ -1,5 +1,36 @@
-var mySwiper = new Swiper('.swiper-container',{
-    pagination: '.pagination',
-    paginationClickable: true,
-    mode: 'vertical'
+"use strict";
+
+var mySlider = new Swiper('.swiper-container',{
+    createPagination: false,
+    mode: 'vertical',
+    onFirstInit: function(swiper){
+    	addEffectForSlide(swiper);
+    	cleanEffectForsiblingSlide(swiper);
+    },
+    onSlideChangeEnd: function(swiper){
+    	addEffectForSlide(swiper);
+    	cleanEffectForsiblingSlide(swiper);
+    }
 });
+
+function addEffectForSlide(swiper){
+
+	var currentSlide = $('.swiper-slide').eq(swiper.activeIndex);
+	currentSlide.find("section").addClass("animated bounceInUp show");
+	
+}
+
+function cleanEffectForsiblingSlide(swiper){
+
+	var currentSildeIndex = swiper.activeIndex;
+	var slides = $('.swiper-slide');
+	
+	if(slides.eq(swiper.activeIndex-1)[0]){
+		slides.eq(swiper.activeIndex-1).find("section").removeClass("animated bounceInUp show");
+	}
+
+	if(slides.eq(swiper.activeIndex+1)[0]){
+		slides.eq(swiper.activeIndex+1).find("section").removeClass("animated bounceInUp show");
+	}
+
+} 
