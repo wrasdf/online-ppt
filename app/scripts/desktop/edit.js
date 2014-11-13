a22bcfdd29785d2a3968894b289f9585bda87c8a
iKerry <wr_asdf@163.com>
2014-11-13 11:30:28 +0800

    [Kerry] feature spike

--- app/scripts/desktop/edit.js
+++ app/scripts/desktop/edit.js
@@ -1,10 +1,13 @@
 "use strict";
 
 $("#convert").bind("click", function(){
+	var test = $(".test");
+	test.css("display","block");
 	html2canvas($(".test"), {
-    onrendered: function(canvas) {
-        var t = document.body.appendChild(canvas);
-        $(t).width(150).height(150);
-    }
+	    onrendered: function(canvas) {
+	        var t = document.body.appendChild(canvas);
+	        $(t).width(150).height(150);
+	        test.css("display","none");
+	    }
+	});
 });
-})
